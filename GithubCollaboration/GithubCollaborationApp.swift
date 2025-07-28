c2a79268d1799cebaa124347f5cf97fa8d85d822
Michelle Lee <mlee1019507@gmail.com>
2025-07-28 09:55:28 -0400

    Initial Commit

---
+++ GithubCollaboration/GithubCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubCollaborationApp.swift
+//  GithubCollaboration
+//
+//  Created by Scholar on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
